c32f74ed795b4e7f172807c50989a23927bba037
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2024-11-13 07:03:43 -0500

    added Java files; Assignment-3

---
+++ Java/Main.java
@@ -0,0 +1,93 @@
+/**
+ * The Main code that runs the 'Triangle' Class.
+ *
+ * @author: Sam Corbett
+ * @version: 1.0
+ * @since: 2024/11/13
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("");
+
+        Triangle testTriangle1 = new Triangle(3, 4, 5);
+        System.out.println("Created a 3 mm by 4 mm by 5 mm triangle.");
+        System.out.println(" → Is the triangle valid: " + testTriangle1.isValid());
+        System.out.println(" → Semiperimeter: " + testTriangle1.semiPerimeter() + " mm");
+        System.out.println(" → Area: " + testTriangle1.area() + " mm²");
+        System.out.println(" → Type: " + testTriangle1.getType());
+        System.out.println(" → Angle 1: " + testTriangle1.angle(1) + " rad");
+        System.out.println(" → Angle 2: " + testTriangle1.angle(2) + " rad");
+        System.out.println(" → Angle 3: " + testTriangle1.angle(3) + " rad");
+        System.out.println(" → Height 1: " + testTriangle1.height(1) + " mm");
+        System.out.println(" → Height 2: " + testTriangle1.height(2) + " mm");
+        System.out.println(" → Height 3: " + testTriangle1.height(3) + " mm");
+        System.out.println(" → Inner circle radius: " + testTriangle1.innerCircleRadius() + " mm");
+        System.out.println(" → Circumcircle radius: " + testTriangle1.circumsicleRadius() + " mm");
+        System.out.println("");
+
+        Triangle testTriangle1 = new Triangle(3, 3, 3);
+        System.out.println("Created a 3 mm by 3 mm by 3 mm triangle.");
+        System.out.println(" → Is the triangle valid: " + testTriangle1.isValid());
+        System.out.println(" → Semiperimeter: " + testTriangle1.semiPerimeter() + " mm");
+        System.out.println(" → Area: " + testTriangle1.area() + " mm²");
+        System.out.println(" → Type: " + testTriangle1.getType());
+        System.out.println(" → Angle 1: " + testTriangle1.angle(1) + " rad");
+        System.out.println(" → Angle 2: " + testTriangle1.angle(2) + " rad");
+        System.out.println(" → Angle 3: " + testTriangle1.angle(3) + " rad");
+        System.out.println(" → Height 1: " + testTriangle1.height(1) + " mm");
+        System.out.println(" → Height 2: " + testTriangle1.height(2) + " mm");
+        System.out.println(" → Height 3: " + testTriangle1.height(3) + " mm");
+        System.out.println(" → Inner circle radius: " + testTriangle1.innerCircleRadius() + " mm");
+        System.out.println(" → Circumcircle radius: " + testTriangle1.circumsicleRadius() + " mm");
+        System.out.println("");
+
+        Triangle testTriangle1 = new Triangle(3, 4, 4);
+        System.out.println("Created a 3 mm by 4 mm by 4 mm triangle.");
+        System.out.println(" → Is the triangle valid: " + testTriangle1.isValid());
+        System.out.println(" → Semiperimeter: " + testTriangle1.semiPerimeter() + " mm");
+        System.out.println(" → Area: " + testTriangle1.area() + " mm²");
+        System.out.println(" → Type: " + testTriangle1.getType());
+        System.out.println(" → Angle 1: " + testTriangle1.angle(1) + " rad");
+        System.out.println(" → Angle 2: " + testTriangle1.angle(2) + " rad");
+        System.out.println(" → Angle 3: " + testTriangle1.angle(3) + " rad");
+        System.out.println(" → Height 1: " + testTriangle1.height(1) + " mm");
+        System.out.println(" → Height 2: " + testTriangle1.height(2) + " mm");
+        System.out.println(" → Height 3: " + testTriangle1.height(3) + " mm");
+        System.out.println(" → Inner circle radius: " + testTriangle1.innerCircleRadius() + " mm");
+        System.out.println(" → Circumcircle radius: " + testTriangle1.circumsicleRadius() + " mm");
+        System.out.println("");
+
+        Triangle testTriangle1 = new Triangle(2, 3, 4);
+        System.out.println("Created a 2 mm by 3 mm by 4 mm triangle.");
+        System.out.println(" → Is the triangle valid: " + testTriangle1.isValid());
+        System.out.println(" → Semiperimeter: " + testTriangle1.semiPerimeter() + " mm");
+        System.out.println(" → Area: " + testTriangle1.area() + " mm²");
+        System.out.println(" → Type: " + testTriangle1.getType());
+        System.out.println(" → Angle 1: " + testTriangle1.angle(1) + " rad");
+        System.out.println(" → Angle 2: " + testTriangle1.angle(2) + " rad");
+        System.out.println(" → Angle 3: " + testTriangle1.angle(3) + " rad");
+        System.out.println(" → Height 1: " + testTriangle1.height(1) + " mm");
+        System.out.println(" → Height 2: " + testTriangle1.height(2) + " mm");
+        System.out.println(" → Height 3: " + testTriangle1.height(3) + " mm");
+        System.out.println(" → Inner circle radius: " + testTriangle1.innerCircleRadius() + " mm");
+        System.out.println(" → Circumcircle radius: " + testTriangle1.circumsicleRadius() + " mm");
+        System.out.println("");
+
+        Triangle testTriangle1 = new Triangle(23, 3, 4);
+        System.out.println("Created a 2 mm by 3 mm by 4 mm triangle.");
+        System.out.println(" → Is the triangle valid: " + testTriangle1.isValid());
+        System.out.println(" → Semiperimeter: " + testTriangle1.semiPerimeter() + " mm");
+        System.out.println(" → Area: " + testTriangle1.area() + " mm²");
+        System.out.println(" → Type: " + testTriangle1.getType());
+        System.out.println(" → Angle 1: " + testTriangle1.angle(1) + " rad");
+        System.out.println(" → Angle 2: " + testTriangle1.angle(2) + " rad");
+        System.out.println(" → Angle 3: " + testTriangle1.angle(3) + " rad");
+        System.out.println(" → Height 1: " + testTriangle1.height(1) + " mm");
+        System.out.println(" → Height 2: " + testTriangle1.height(2) + " mm");
+        System.out.println(" → Height 3: " + testTriangle1.height(3) + " mm");
+        System.out.println(" → Inner circle radius: " + testTriangle1.innerCircleRadius() + " mm");
+        System.out.println(" → Circumcircle radius: " + testTriangle1.circumsicleRadius() + " mm");
+        System.out.println("");
+    }
+}
---
+++ Java/Triangle.java
@@ -0,0 +1,194 @@
+/**
+ * The 'Triangle' Class.
+ *
+ * @author: Sam Corbett
+ * @version: 1.0
+ * @since: 2024/11/13
+ */
+
+/**
+ * Class representing a triangle.
+ */
+public class Triangle {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    /**
+     * Create a triangle.
+     * @param side1 The length of the first side.
+     * @param side2 The length of the second side.
+     * @param side3 The length of the third side.
+     */
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    /**
+     * Get the length of the first side.
+     * @return The length of the first side.
+     */
+    public double getSide1() {
+        return side1;
+    }
+
+    /**
+     * Get the length of the second side.
+     * @return The length of the second side.
+     */
+    public double getSide2() {
+        return side2;
+    }
+
+    /**
+     * Get the length of the third side.
+     * @return The length of the third side.
+     */
+    public double getSide3() {
+        return side3;
+    }
+
+    /**
+     * Calculate the perimeter of the triangle.
+     * @return The perimeter of the triangle.
+     */
+    private double perimeter() {
+        return side1 + side2 + side3;
+    }
+
+    /**
+     * Check if the triangle is valid.
+     * @return True if the triangle is valid, false otherwise.
+     */
+    public boolean isValid() {
+        return (side1 + side2 > side3 &&
+                side2 + side3 > side1 &&
+                side3 + side1 > side2);
+    }
+
+    /**
+     * Calculate the area of the triangle.
+     * @return The area of the triangle, or -1 if the triangle is not valid.
+     */
+    public double area() {
+        if (!isValid()) return -1;
+        double semiPerimeter = semiPerimeter();
+        return Math.sqrt(semiPerimeter 
+          * (semiPerimeter - side1)
+          * (semiPerimeter - side2) 
+          * (semiPerimeter - side3)
+        );
+    }
+
+    /**
+     * Get the type of the triangle.
+     * @return The type of the triangle.
+     */
+    public String getType() {
+        if (!isValid()) {
+            return "Invalid";
+        } else if (side1 == side2 && side2 == side3) {
+            return "Equilateral Triangle";
+        } else if (side1 == side2 || side2 == side3 || side3 == side1) {
+            if (isRightAngle()) {
+                return "Right Icosceles Triangle";
+            } else {
+                return "Isosceles Triangle";
+            }
+        } else {
+            if (isRightAngle()) {
+                return "Right Scalene Triangle";
+            } else {
+                return "Scalene Triangle";
+            }
+        }
+    }
+
+    /**
+     * Check if the triangle is a right angle triangle.
+     * @return True if the triangle is a right angle triangle,
+     * false otherwise.
+     */
+    private boolean isRightAngle() {
+        double a2 = Math.pow(side1, 2);
+        double b2 = Math.pow(side2, 2);
+        double c2 = Math.pow(side3, 2);
+        return (a2 + b2 == c2 || b2 + c2 == a2 || c2 + a2 == b2);
+    }
+
+    /**
+     * Calculate the semi-perimeter of the triangle.
+     * @return The semi-perimeter of the triangle.
+     */
+    public double semiPerimeter() {
+        if (!isValid()) return -1;
+        return perimeter() / 2;
+    }
+
+    /**
+     * Calculate the specified angle of the triangle.
+     * @param angleNumber The angle number (1, 2, or 3).
+     * @return The angle in radians, or -1 if the triangle is not valid.
+     */
+    public double angle(int angleNumber) {
+        if (!isValid()) return -1;
+        double a2 = Math.pow(side1, 2);
+        double b2 = Math.pow(side2, 2);
+        double c2 = Math.pow(side3, 2);
+        if (angleNumber == 1) {
+            return Math.acos((b2 + c2 - a2) / (2 * side2 * side3));
+        } else if (angleNumber == 2) {
+            return Math.acos((a2 + c2 - b2) / (2 * side1 * side3));
+        } else if (angleNumber == 3) {
+            return Math.acos((a2 + b2 - c2) / (2 * side1 * side2));
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Calculate the height relative to the specified side of the triangle.
+     * @param sideNumber The side number (1, 2, or 3).
+     * @return The height in millimeters, or -1 if the triangle is
+     * not valid.
+     */
+    public double height(int sideNumber) {
+        if (!isValid()) return -1;
+        double area = area();
+        if (sideNumber == 1) {
+            return (2 * area) / side1;
+        } else if (sideNumber == 2) {
+            return (2 * area) / side2;
+        } else if (sideNumber == 3) {
+            return (2 * area) / side3;
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Calculate the radius of the largest inscribed circle within the triangle.
+     * @return The radius in millimeters, or -1 if the triangle is not valid.
+     */
+    public double innerCircleRadius() {
+        if (!isValid()) return -1;
+        double area = area();
+        double semiPerimeter = semiPerimeter();
+        return area / semiPerimeter;
+    }
+
+    /**
+     * Calculate the radius of the circumscribed circle around the triangle.
+     * @return The radius in millimeters, or -1 if the triangle is not valid.
+     */
+    public double circumsicleRadius() {
+        if (!isValid()) return -1;
+        double a = side1;
+        double b = side2;
+        double c = side3;
+        double area = area();
+        return (a * b * c) / (4 * area);
+    }
+}
