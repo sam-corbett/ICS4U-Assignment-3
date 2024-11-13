4c7440def516b0bc85bff7103d96eb41d1320fbd
Sam Corbett <sam.corbett@mths.ca>
2024-11-13 14:57:22 +0000

    Fixing stuff from Triangle.java; Assignment-2

--- Java/Triangle.java
+++ Java/Triangle.java
@@ -6,8 +6,18 @@
  * @since: 2024/11/13
  */
 public class Triangle {
+    /**
+     * The length of the first side.
+     */
     private double side1;
+
+    /**
+     * The length of the second side.
+     */
     private double side2;
+    /**
+     * The length of the third side.
+     */
     private double side3;
 
     /**
@@ -17,9 +27,7 @@ public class Triangle {
      * @param side3 - The length of the third side.
      */
     public Triangle(
-        final double side1,
-        final double side2,
-        final double side3
+        final double side1, final double side2, final double side3
     ) {
         this.side1 = side1;
         this.side2 = side2;
